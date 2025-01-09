21c2cbf6f4c99bff720935e013aac39485c14cfb
nstgn <139435736+nstgn@users.noreply.github.com>
2025-01-09 14:44:33 +0700

    Add files via upload

---
+++ streamlit_app.py
@@ -0,0 +1,9 @@
+import streamlit as st
+from streamlit_gsheets import GSheetsConnection
+
+url = "https://docs.google.com/spreadsheets/d/1SczaIV1JHUSca1hPilByJFFzOi5a8Hkhi0OemlmPQsY/edit?usp=sharing"
+conn = st.connection("gsheets", type=GSheetsConnection)
+
+#data = conn.read(worksheet="Sheet1")
+data = conn.read(spreadsheet=url, usecols=[0, 1, 2])
+st.dataframe(data)
