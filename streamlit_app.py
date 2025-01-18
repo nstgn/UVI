74037a4c5cac40f14a8396017fb38cbb366902fc
nstgn <139435736+nstgn@users.noreply.github.com>
2025-01-18 10:37:10 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -5,5 +5,5 @@ url = "https://docs.google.com/spreadsheets/d/1SczaIV1JHUSca1hPilByJFFzOi5a8Hkhi
 conn = st.connection("gsheets", type=GSheetsConnection)
 
 #data = conn.read(worksheet="Sheet1")
-data = conn.read(spreadsheet=url, usecols=[0, 1, 2])
+data = conn.read(spreadsheet=url, usecols=[0, 1, 2, 3])
 st.dataframe(data)
