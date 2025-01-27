789b74b13adc765f0d48e429058c1aa0c888ea6d
nstgn <139435736+nstgn@users.noreply.github.com>
2025-01-27 08:36:44 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -70,8 +70,10 @@ test_predicted = model.predict(X_test)
 last_time = data.index[-1]
 last_time = last_time.replace(second=0, microsecond=0)
 minute_offset = last_time.minute % 30
-if minute_offset != 0:
-    last_time += pd.Timedelta(minutes=(30 - minute_offset))
+if minute_offset < 15:
+  last_time -= pd.Timedelta(minutes=minute_offset)
+else:
+  last_time += pd.Timedelta(minutes=(30 - minute_offset))
 
 # Interval waktu 30 menit
 time_interval = pd.Timedelta(minutes=30)
