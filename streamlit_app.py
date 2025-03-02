f17d3eda3d6636ab970487caa4f2b4ee001f35bd
nstgn <139435736+nstgn@users.noreply.github.com>
2025-03-02 15:45:02 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -122,7 +122,8 @@ st.markdown(
 # Membuat gauge chart
 latest_data = data.iloc[-1] 
 latest_time = latest_data.name 
-uv_index = latest_data['Index'] 
+#uv_index = latest_data['Index'] 
+uv_index = latest_data['0'] 
 
 fig = go.Figure(go.Indicator(
     mode="gauge+number",
